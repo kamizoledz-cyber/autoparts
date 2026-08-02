227d8812a8d357d5d5197864e70bbb730ffc965c
kamizoledz-cyber <kamizoledz@gmail.com>
2026-08-02 19:24:40 +0100

    Update app.js

--- assets/css/assets/js/app.js
+++ assets/css/assets/js/app.js
@@ -1,11 +1,1 @@
-const app = document.getElementById("app");
-
-app.innerHTML = `
-<div class="container">
-    <h1>🚗 GAPHub</h1>
-
-    <p>
-        Global Auto Parts & Compatibility Hub
-    </p>
-</div>
-`;
+alert("JavaScript Works!");
