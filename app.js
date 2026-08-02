2967b011787a70acf0bf5fce2bcab81e028559d9
kamizoledz-cyber <kamizoledz@gmail.com>
2026-08-02 20:00:09 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log("GAPHub Started");
+
+const app = document.getElementById("app");
+
+app.innerHTML = `
+<header class="header">
+    <h1>🚗 GAPHub</h1>
+    <p>Global Auto Parts & Compatibility Hub</p>
+</header>
+`;
