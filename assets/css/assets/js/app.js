8521a8b5fef879295baf7e0575de9be6a13c7fce
kamizoledz-cyber <kamizoledz@gmail.com>
2026-08-02 08:26:36 +0100

    Create app.js

---
+++ assets/css/assets/js/app.js
@@ -0,0 +1,11 @@
+const app = document.getElementById("app");
+
+app.innerHTML = `
+<div class="container">
+    <h1>🚗 GAPHub</h1>
+
+    <p>
+        Global Auto Parts & Compatibility Hub
+    </p>
+</div>
+`;
